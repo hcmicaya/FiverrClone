115738218e00cd0b5a498ff776c744b83df8f98f
Hans Micaya <hcmicaya@gmail.com>
2023-06-28 23:24:08 -0400

    Added slide and catCard feature

---
+++ src/components/slide/Slide.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Slide.scss";
+import Slider from "infinite-react-carousel";
+
+const Slide = ({ children, slidesToShow, arrowScroll }) => {
+    return (
+        <div className="slide">
+            <div className="container">
+                <Slider slidesToShow={slidesToShow} arrowsScroll={arrowScroll}>
+                    {children}
+                </Slider>
+            </div>
+        </div>
+    );
+};
+
+export default Slide;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
 
 const Home = () => {
-    return <div className="home">Home</div>;
+    return (
+        <div className="home">
+            <Featured />
+            <TrustedBy />
+            <Slide slidesToShow={5} arrowScroll={5}>
+                {cards.map((card) => (
+                    <CatCard key={card.id} item={card} />
+                ))}
+            </Slide>
+        </div>
+    );
 };
 
 export default Home;
